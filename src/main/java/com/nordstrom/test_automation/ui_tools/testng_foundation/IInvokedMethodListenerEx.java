24add4fbb184e72ab97640d1670ec84698c7bd80
BB8D <BB8D@MW00973298.nordstrom.net>
2017-01-06 15:16:36 -0800

    Add listener chain and invoked method hook. Add JavaDoc.

---
+++ src/main/java/com/nordstrom/test_automation/ui_tools/testng_foundation/IInvokedMethodListenerEx.java
@@ -0,0 +1,7 @@
+package com.nordstrom.test_automation.ui_tools.testng_foundation;
+
+import org.testng.IInvokedMethodListener;
+
+public interface IInvokedMethodListenerEx extends IInvokedMethodListener {
+
+}
---
+++ src/main/java/com/nordstrom/test_automation/ui_tools/testng_foundation/ListenerChainable.java
@@ -0,0 +1,7 @@
+package com.nordstrom.test_automation.ui_tools.testng_foundation;
+
+public interface ListenerChainable {
+	
+	public void attachListeners(ListenerChain listenerChain);
+	
+}
