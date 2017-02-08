e964b8deccd334c57afd56d4ceed33573bae7fc3
BB8D <BB8D@mw00973298.nordstrom.net>
2017-02-07 19:11:03 -0800

    Remove getters/setters for Selenium driver property

--- src/main/java/com/nordstrom/automation/testng/PropertyManager.java
+++ src/main/java/com/nordstrom/automation/testng/PropertyManager.java
@@ -2,9 +2,7 @@ package com.nordstrom.automation.testng;
 
 import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
-import org.testng.Reporter;
 import org.testng.collections.Maps;
 
 /**
@@ -14,62 +12,14 @@ import org.testng.collections.Maps;
  *         into a {@link Map}.</li>
  *     <li>{@link #injectAttributes(Map, ITestResult)} injects the entries of the specified {@link Map} into the
  *         attribute collection of the specified test result.</li>
- *     <li>There are also getters/setters for drivers attached to specific tests: 
- *         <ul>
- *             <li>{@link #getDriver()} gets the driver for the current test.</li>
- *             <li>{@link #setDriver(WebDriver)} sets the driver for the current test.</li>
- *             <li>{@link #getDriver(ITestResult)} gets the driver for the specified test result.</li>
- *             <li>{@link #setDriver(WebDriver, ITestResult)} sets the driver for the specified test result.</li>
- *         </ul>
- *     </li>
  * </ul>
  */
 public class PropertyManager {
-	
-	private static final String DRIVER = "DRIVER";
 	
 	private PropertyManager() {
 		throw new AssertionError("PropertyManager is a static utility class.");
 	}
 	
-	/**
-	 * Get the driver for the current test
-	 * 
-	 * @return driver for the current test
-	 */
-	public static WebDriver getDriver() {
-		return getDriver(Reporter.getCurrentTestResult());
-	}
-	
-	/**
-	 * Set the driver for the current test
-	 * 
-	 * @param driver driver for the current test
-	 */
-	public static void setDriver(WebDriver driver) {
-		setDriver(driver, Reporter.getCurrentTestResult());
-	}
-	
-	/**
-	 * Get the driver for the specified test result
-	 * 
-	 * @param testResult test result object
-	 * @return driver from the specified test result
-	 */
-	public static WebDriver getDriver(ITestResult testResult) {
-		return (WebDriver) testResult.getAttribute(DRIVER);
-	}
-	
-	/**
-	 * Set the driver for the specified test result
-	 * 
-	 * @param driver test result object
-	 * @param testResult driver for the specified test result
-	 */
-	public static void setDriver(WebDriver driver, ITestResult testResult) {
-		testResult.setAttribute(DRIVER, driver);
-	}
-
 	/**
 	 * Extract the attribute collection from the specified test result into a {@link Map}.
 	 * 
