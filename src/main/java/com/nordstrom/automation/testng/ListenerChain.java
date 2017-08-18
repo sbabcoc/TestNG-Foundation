479555181aff668cb5992709a7ff9bde100df6c5
BB8D <BB8D@MW00973298.nordstrom.net>
2017-08-18 16:27:51 -0700

    Fix JavaDoc formatting

--- src/main/java/com/nordstrom/automation/testng/ListenerChain.java
+++ src/main/java/com/nordstrom/automation/testng/ListenerChain.java
@@ -315,8 +315,8 @@ public class ListenerChain implements ISuiteListener, ITestListener, IClassListe
     /**
      * [IAnnotationTransformer]
      * This method will be invoked by TestNG to give you a chance to modify a TestNG annotation read from your test
-     * classes. You can change the values you need by calling any of the setters on the ITest interface. Note that only one of the three parameters
-     * testClass, testConstructor and testMethod will be non-null.
+     * classes. You can change the values you need by calling any of the setters on the ITest interface. Note that
+     * only one of the three parameters testClass, testConstructor and testMethod will be non-null.
      * 
      * @param annotation The annotation that was read from your test class.
      * @param testClass If the annotation was found on a class, this parameter represents this class
@@ -366,14 +366,16 @@ public class ListenerChain implements ISuiteListener, ITestListener, IClassListe
     }
 
     /**
-     * [IAnnotationTransformer2]
-     * Transform an IConfiguration annotation. Note that only one of the three parameters testClass, testConstructor
-     * and testMethod will be non-null.
+     * [IAnnotationTransformer2] Transform an IConfiguration annotation. Note that only one of the
+     * three parameters testClass, testConstructor and testMethod will be non-null.
      * 
      * @param annotation The annotation that was read from your test class.
-     * @param testClass If the annotation was found on a class, this parameter represents this class (null otherwise).
-     * @param testConstructor If the annotation was found on a constructor, this parameter represents this constructor (null otherwise).
-     * @param testMethod If the annotation was found on a method, this parameter represents this method (null otherwise).
+     * @param testClass If the annotation was found on a class, this parameter represents this class
+     *        (null otherwise).
+     * @param testConstructor If the annotation was found on a constructor, this parameter
+     *        represents this constructor (null otherwise).
+     * @param testMethod If the annotation was found on a method, this parameter represents this
+     *        method (null otherwise).
      */
     @Override
     @SuppressWarnings("rawtypes")
