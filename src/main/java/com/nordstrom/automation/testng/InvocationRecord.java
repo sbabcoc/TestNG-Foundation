9e6ac15431820822930b401960fcc3b93deacc5c
Scott Babcock <scoba@hotmail.com>
2018-04-26 22:10:30 -0700

    More JavaDoc

--- src/main/java/com/nordstrom/automation/testng/InvocationRecord.java
+++ src/main/java/com/nordstrom/automation/testng/InvocationRecord.java
@@ -7,7 +7,25 @@ import java.util.Arrays;
 import org.testng.ITestResult;
 
 /**
+ * This class captures the details of a single invocation of a TestNG test method. Objects of this type are used by
+ * {@link RetryManager} to track the number of retries that remain for each unique method invocation. In addition to
+ * providing a unique identifier for specific methods and parameter values, <b>InvocationRecord</b> also provides a
+ * {@link #toString} method that emits a string in the following format:
+ * <blockquote>{@code className.methodName(parmValue...)}</blockquote>
  * 
+ * The content of each {@code parmValue} item (if any) represents the actual value passed to the corresponding
+ * argument of a failed invocation of a parameterized test. Sensitive values can be redacted by marking their test
+ * method arguments with the {@link RedactValue} annotation:
+ * 
+ * <blockquote><pre>
+ * &commat;Test
+ * &commat;Parameters({"username", "password"})
+ * public void testLogin(String username, &commat;RedactValue String password) {
+ *     // test implementation goes here
+ * }</pre></blockquote>
+ * 
+ * The retry message for this method would include the actual user name, but redact the password:
+ * <blockquote>{@code ### RETRY ### [MySuite/MyTest] AccountTest.testLogin(john.doe, |:password:|)}</blockquote>
  */
 class InvocationRecord {
     
@@ -19,8 +37,9 @@ class InvocationRecord {
     final Object[] parameters;
     
     /**
+     * Constructor: Initialize invocation record for the specific test result.
      * 
-     * @param result
+     * @param result TestNG test result object
      */
     InvocationRecord(ITestResult result) {
         method = result.getMethod().getConstructorOrMethod().getMethod();
