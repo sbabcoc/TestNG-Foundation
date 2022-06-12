e4729315f5cca14229e2009b1ffa970fb777bd51
Scott Babcock <scoba@hotmail.com>
2022-06-11 18:58:35 -0700

    Build for Java 8; upgrade dependencies and plugins

--- src/test-j7/java/com/nordstrom/automation/testng/ChainedListener.java
+++
@@ -1,9 +0,0 @@
-package com.nordstrom.automation.testng;
-
-import org.testng.IAnnotationTransformer3;
-import org.testng.IConfigurationListener2;
-
-public class ChainedListener extends AbstractChainedListener
-        implements IAnnotationTransformer3, IConfigurationListener2 {
-    
-}
